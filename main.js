b1d8317f22b334170f42529a0f602d2f6ffba2a2
HOT HEAD <hothead01th@gmail.com>
2023-08-06 17:46:56 +0530

    Copyright (c) HOTHEAD01TH. All rights reserved.

---
+++ main.js
@@ -0,0 +1,3 @@
+// Copyright (c) HOTHEAD01TH. All rights reserved.
+// File: main.js
+// Description: lets collaborate and build something extraordinary!.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        "xik" : "#171219",
+        "coffee" : "#3A3335",
+        "rich-black" : "#000505"
+      },
+      fontFamily : {
+        "poppins" : ['poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
